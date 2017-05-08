2327ce04ab19ef4b4ce9dac39a0fe3b256e35a2b
João Filipe Vaz <joaofilipevaz@gmail.com>
2017-05-08 21:53:41 +0100

    wip

--- src/agent_prosp/agente_prosp.py
+++ src/agent_prosp/agente_prosp.py
@@ -1,20 +1,21 @@
+
 class AgenteProspector(Agente):
 
     def __init__(self, controlo):
-        self._controlo = controlo
+        self.__controlo = controlo
 
     def executar(self):
         percepcao = self._percepcionar()
         accao = self._processar(percepcao)
         if accao is not None:
-            self._actuar(accao)
+            self.__actuar(accao)
 
-    def _percepcionar(self):
+    def __percepcionar(self):
         return self.sensor_multiplo.detectar()
 
-    def _processar(self, percepcao):
-        return self._controlo.processar(percepcao)
+    def __processar(self, percepcao):
+        return self.__controlo.processar(percepcao)
 
-    def _actuar(self):
+    def __actuar(self):
         return self.actuador.actuar(accao)
 
