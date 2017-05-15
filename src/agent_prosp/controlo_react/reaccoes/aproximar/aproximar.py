a5700974ba103ec158af1af936cd407385da79ce
João Filipe Vaz <joaofilipevaz@gmail.com>
2017-05-15 22:11:02 +0100

    wip

--- src/agent_prosp/controlo_react/reaccoes/aproximar/aproximar.py
+++ src/agent_prosp/controlo_react/reaccoes/aproximar/aproximar.py
@@ -1,2 +1,11 @@
+from psa.psa5.actuador import FRT, DIR, ESQ
+from agent_prosp.controlo_react.reaccoes.aproximar.aproximar_dir import AproximarDir
+from lib.ecr.prioridade import Prioridade
 
-class Aproximar():
+
+class Aproximar(Prioridade):
+
+    def __init__(self):
+        super(Aproximar, self).__init__(
+            [AproximarDir(FRT), AproximarDir(ESQ), AproximarDir(DIR)]
+        )
--- src/agent_prosp/controlo_react/reaccoes/aproximar/aproximar_dir.py
+++ src/agent_prosp/controlo_react/reaccoes/aproximar/aproximar_dir.py
@@ -1,2 +1,18 @@
+from psa.psa5.accao import Mover
+from lib.ecr.reaccao import Reaccao
+from lib.ecr.resposta import Resposta
 
-class Aproximar_dir():
+
+class AproximarDir(Reaccao):
+
+    def __init__(self, direccao):
+        self.__direccao = direccao
+        self.__accao = Mover(direccao)
+
+    def detectar_estimulo(self, percepcao):
+        if percepcao[self.__direccao].alvo:
+            return percepcao[self.__direccao].distancia
+
+    def gerar_resposta(self, estimulo):
+        prioridade = 1 / (1 + estimulo)
+        return Resposta(self.__accao, prioridade)
