d2b0249de47fcfd11f0e5a55b60eeaebd8ac34c7
João Filipe Vaz <joaofilipevaz@gmail.com>
2017-05-15 21:30:54 +0100

    wip

--- src/agent_prosp/controlo_react/reaccoes/evitar.py
+++ src/agent_prosp/controlo_react/reaccoes/evitar.py
@@ -1,3 +1,7 @@
 
 
-class Evitar(reaccao):
+class Evitar(Reaccao):
+    def detectar_estimulo(self, percepcao):
+
+
+    def gerar_resposta:
--- src/agent_prosp/controlo_react/reaccoes/explorar.py
+++ src/agent_prosp/controlo_react/reaccoes/explorar.py
@@ -1,3 +1,12 @@
+from psa.psa5.accao import Mover
+from psa.psa5.actuador import ESQ, DIR, FRT
+from random import choice
+
+from lib.ecr.comportamento import Comportamento
+from lib.ecr.resposta import Resposta
 
 
-class Explorar():
+class Explorar(Comportamento):
+    def activar(self, percepcao):
+        accao = choice([Mover(ESQ), Mover(FRT), Mover(DIR)])
+        return Resposta(accao)
