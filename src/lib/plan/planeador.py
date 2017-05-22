b8320102b4e285cfe55326989631806acb4577f3
João Filipe Vaz <joaofilipevaz@gmail.com>
2017-05-22 21:43:50 +0100

    wip

---
+++ src/lib/plan/planeador.py
@@ -0,0 +1,21 @@
+import abc
+import modelo_plan
+
+class Planeador:
+    _metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def planear(self, modelo_plan, estado_inicial, objectivos):
+        abstract
+
+    @abc.abstractmethod
+    def obter_accao(self, estado):
+        abstract
+
+    @abc.abstractmethod
+    def plano_pendente(self):
+        abstract
+
+    @abc.abstractmethod
+    def terminar_plano(self):
+        abstract
