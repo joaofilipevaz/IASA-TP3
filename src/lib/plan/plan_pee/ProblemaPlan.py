6656e281a83d348b0996b6ddb74e182fe44ef83c
João Filipe Vaz <joaofilipevaz@gmail.com>
2017-05-29 21:36:17 +0100

    wip

--- src/lib/plan/plan_pee/ProblemaPlan.py
+++ src/lib/plan/plan_pee/ProblemaPlan.py
@@ -1,10 +1,17 @@
+from lib.pee.modprob.problema_heur import ProblemaHeur
+from psa.util import dist
 
 class ProblemaPlan(ProblemaHeur):
 
     def __init__(self, estado_inicial, estado_final, operadores):
+        super(ProblemaPlan, self).__init__()
         self.__estado_final = estado_final
 
     def objectivo(self, estado):
+        if estado == self.__estado_final:
+            return True
+        else:
+            return False
 
     def heuristica(self, estado):
-
+        return dist(estado, self.__estado_final)
