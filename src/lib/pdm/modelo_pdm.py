34bc99c5b3650d10107aae52ef98c3767968011c
João Filipe Vaz <joaofilipevaz@gmail.com>
2017-06-06 00:19:52 +0100

    wip

--- src/lib/pdm/modelo_pdm.py
+++ src/lib/pdm/modelo_pdm.py
@@ -1,13 +1,21 @@
+import abc
 
 
 class ModeloPDM:
+    __metaclass__ = abc.ABCMeta
 
+    @abc.abstractmethod
     def S(self):
+        """lista de estados"""
 
-
+    @abc.abstractmethod
     def A(self, estado):
+        """lista de operadores"""
 
-
+    @abc.abstractmethod
     def T(self, s, a):
+        """lista de transicoes"""
 
+    @abc.abstractmethod
     def R(self, s, a, sn):
+        """resposta"""
