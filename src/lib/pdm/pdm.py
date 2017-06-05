91315299f05af66ea7e9a3e67dc447ccece0e3cc
João Filipe Vaz <joaofilipevaz@gmail.com>
2017-06-05 22:30:36 +0100

    wip

--- src/lib/pdm/pdm.py
+++ src/lib/pdm/pdm.py
@@ -11,10 +11,22 @@ class Pdm(ModeloPDM):
     def utilidade(self, modelo):
         S = modelo.S
         A = modelo.A
+        R = modelo.R
+        T = modelo.T
+        gama = self.__gama
         U = { s:0 for s in S()}
         while True:
             Uant = U.copy()
             delta = 0
             for s in S():
-                max()
+                U[s] = max(self.util_accao(s, a, Uant, modelo)) for a in A(s)
+                delta = max(delta, abs(U[s] - Uant[s]))
+            # se o delta for menor que o delta max
+            if delta < self.__delta_max:
+                break
+
+
+    def util_Accao(self, s, a, U, modelo):
+
+                U[s] = max(p*R(s, a, sn) + gama*Uant[sn]) for ()
 
