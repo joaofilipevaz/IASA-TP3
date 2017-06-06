dc138065fd633494d9bcd2fe1b2bee6d717e05d1
João Filipe Vaz <joaofilipevaz@gmail.com>
2017-06-06 01:10:16 +0100

    wip

---
+++ src/lib/plan/plan_pdm/modelo_pdm_plan.py
@@ -0,0 +1,27 @@
+
+
+class ModeloPDMPlan():
+
+
+    def __init__(self):
+
+    def __iniciar_modelo(self, modelo_plan):
+
+    def __gerar_modelo(self, s, a):
+
+    def __gerar_recompensa(self, s, a, sn):
+
+    def estados(self):
+
+    def operadores(self):
+
+    def S(self):
+
+    def A(self, estado):
+
+    def T(self, s, a):
+
+    def R(self, s, a, sn):
+
+
+
---
+++ src/lib/plan/plan_pdm/plan_pdm.py
@@ -0,0 +1,21 @@
+from lib.plan.planeador import Planeador
+
+class PlanPDM(Planeador):
+
+    def __init__(self):
+
+
+    def planear(self, modelo_plan, estado_inicial, objectivos):
+
+
+    def obter_accao(self, estado):
+
+
+    def plano_pendente(self):
+
+
+    def terminar_plano(self):
+
+
+
+
