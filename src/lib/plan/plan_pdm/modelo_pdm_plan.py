e0c74e35489a0cc6e8c1755fd12c7a260a15c82e
João Filipe Vaz <joaofilipevaz@gmail.com>
2017-06-11 17:03:43 +0100

    wip

--- src/lib/plan/plan_pdm/modelo_pdm_plan.py
+++ src/lib/plan/plan_pdm/modelo_pdm_plan.py
@@ -2,26 +2,43 @@
 
 class ModeloPDMPlan():
 
-
-    def __init__(self):
+    #def __init__(self):
 
     def __iniciar_modelo(self, modelo_plan):
+        self.__S=modelo_plan.estados()
+        self.__A=modelo_plan.operadores()
+        for s in self.__S:
+            for a in self.__A:
+                self.__gerar_modelo(s,a)
 
     def __gerar_modelo(self, s, a):
+        sn = a.aplicar(s)
+        if sn is None:
+            self.__T[(s,a)]=[]
+        else:
+            self.__T[(s,a)]=[(1,sn)]
+            self.__R[(s,a,sn)]= self.__gerar_recompensa(s,a,sn)
 
     def __gerar_recompensa(self, s, a, sn):
+        r = -a.custo(s,sn)
+        if sn in self.__objectivos:
+            r += self.rmax
+        return r
 
     def estados(self):
+        return self.__S
 
     def operadores(self):
+        return self.__A
 
     def S(self):
+        return self.__S
 
     def A(self, estado):
+        return self.__A(estado)
 
     def T(self, s, a):
+        return self.__T[(s, a)]
 
     def R(self, s, a, sn):
-
-
-
+        return self.__R[(s, a, sn)]
