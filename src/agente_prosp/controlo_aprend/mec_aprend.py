68ddb9a00f6d72d4fe18ed8c9d55df95d8645894
João Filipe Vaz <joaofilipevaz@gmail.com>
2017-06-16 19:51:10 +0100

    wip

--- src/agente_prosp/controlo_aprend/mec_aprend.py
+++ src/agente_prosp/controlo_aprend/mec_aprend.py
@@ -1,18 +1,27 @@
 from lib.aprend_ref.memoria_aprend import MemoriaAprend
 from lib.aprend_ref.aprend_ref import AprendRef
+from lib.aprend_ref.memoria_esparsa import MemoriaEsparsa
 from lib.aprend_ref.sel_accao import SelAccao
+from lib.aprend_ref.aprend_q import AprendQ
+from lib.aprend_ref.sel_accao_greddy import SelAccaoEGreedy
+import psa.psa5 as psa
 
 
 class MecAprend(SelAccao):
 
-    def __init__(self, accoes):
+    def __init__(self, accoes, alfa=1, gama=0.9, epsilon=0.01, nsim=100):
         self.__accoes = accoes
-        self.__mem_aprend = MemoriaAprend()
-        self.__sel_accao = SelAccao()
-        self.__aprend_ref = AprendRef(self.__mem_aprend, self.__sel_accao)
+        self.__mem_aprend = MemoriaEsparsa()
+        self.__sel_accao = SelAccaoEGreedy(self.__mem_aprend, self.__accoes, epsilon)
+        self.__aprend_ref = AprendQ(self.__mem_aprend, self.__sel_accao, alfa, gama)
 
     def aprender(self, s, a, r, sn):
         pass
 
     def seleccionar_accao(self, s):
         pass
+
+    def mostrar(self, s):
+        psa.vismod.limpar()
+        psa.vismod.aprendref(self.__aprend_ref)
+        psa.visvec.accoesestado(s, self.__accoes, self.__mem_aprend.memoria)
