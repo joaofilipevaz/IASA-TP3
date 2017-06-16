2532331bd2e10a9aaa3d6957674c620c927b758f
João Filipe Vaz <joaofilipevaz@gmail.com>
2017-06-16 19:15:39 +0100

    wip

--- src/lib/aprend_ref/sel_accao_greddy.py
+++ src/lib/aprend_ref/sel_accao_greddy.py
@@ -1,5 +1,6 @@
 from lib.aprend_ref.memoria_aprend import MemoriaAprend
 from lib.aprend_ref.sel_accao import SelAccao
+from random import random, choice, shuffle
 
 
 class SelAccaoEGreedy(MemoriaAprend, SelAccao):
@@ -7,12 +8,17 @@ class SelAccaoEGreedy(MemoriaAprend, SelAccao):
     def __init__(self, mem_aprend, accoes, epsilon):
         self.__epsilon = epsilon
         self.__mem_aprend = mem_aprend
+        self.__accoes = accoes
 
     def seleccionar_accao(self, s):
-        pass
+        if random() > self.__epsilon:
+            accao = self.max_accao(s)
+        else:
+            accao = self.explorar(s)
+        return accao
 
     def max_accao(self, s):
-        pass
+        shuffle(self.__accoes)
 
     def explorar(self, s):
-        pass
+        choice(self.__accoes)
