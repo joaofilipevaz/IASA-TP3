5be71ce75b350468acc372cca8e6357fdae2c1e1
João Filipe Vaz <joaofilipevaz@gmail.com>
2017-06-16 20:05:36 +0100

    wip

--- src/agente_prosp/controlo_aprend/controlo_aprend_ref.py
+++ src/agente_prosp/controlo_aprend/controlo_aprend_ref.py
@@ -1,16 +1,22 @@
 from agente_prosp.controlo import Controlo
-
+from agente_prosp.controlo_aprend.mec_aprend import MecAprend
+from psa.util import dirmov
 
 class ControloAprendRef(Controlo):
 
     def __init__(self):
-        self.__rmax =
-        self.__s =
-        self.__a =
-        self.__mec_aprend
+        self.__rmax = 100
+        self.__s = None
+        self.__a = dirmov()
+        self.__mec_aprend = MecAprend(self.__a)
 
     def processar(self, percepcao):
         pass
 
     def __gerar_reforco(self, percepcao):
-        pass
+        r = -percepcao.custo_mov
+        if percepcao.carga:
+            r += self.__rmax
+        elif percepcao.colisao:
+            r += -self.__rmax
+        return r
