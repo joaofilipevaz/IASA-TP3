877a1763152076a5e1eb0ae979ccbe90527e561a
joaofilipevaz@gmail.com <vicio69>
2017-07-23 01:32:37 +0100

    final

--- src/teste/teste_pdm.py
+++ src/teste/teste_pdm.py
@@ -8,5 +8,5 @@ from agente_prosp.agente_prosp import AgenteProspector
 from agente_prosp.controlo_delib.controlo_delib import ControloDelib
 from lib.plan.plan_pdm.plan_pdm import PlanPDM
 
-psa.iniciar("amb/amb1.das")
+psa.iniciar("amb/amb2.das")
 psa.executar(AgenteProspector(ControloDelib(PlanPDM())))
