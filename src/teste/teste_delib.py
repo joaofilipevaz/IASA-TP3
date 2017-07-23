9d9e233f22037c954936ea0f2ac14cd584ff3667
joaofilipevaz@gmail.com <vicio69>
2017-07-23 01:42:49 +0100

    final

--- src/teste/teste_delib.py
+++ src/teste/teste_delib.py
@@ -3,11 +3,12 @@ import sys
 
 sys.path.append("../agente_prosp")
 sys.path.append("../lib")
+sys.path.append("../amb")
 
 from agente_prosp.agente_prosp import AgenteProspector
 from agente_prosp.controlo_delib.controlo_delib import ControloDelib
 from lib.plan.plan_pee.plan_pee import PlanPEE
 from pee.melhorprim.procura_aa import ProcuraAA
 
-psa.iniciar("amb/amb5.das")
+psa.iniciar("amb/amb1.das")
 psa.executar(AgenteProspector(ControloDelib(PlanPEE(ProcuraAA()))))
--- src/teste/teste_pdm.py
+++ src/teste/teste_pdm.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append("../agente_prosp")
 sys.path.append("../lib")
 
-from agente_prosp.agente_prosp import AgenteProspector
+from agente_prosp.agente_prosp import AgenteProspectors
 from agente_prosp.controlo_delib.controlo_delib import ControloDelib
 from lib.plan.plan_pdm.plan_pdm import PlanPDM
 
